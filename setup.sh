e9ad4568038171a7e993cf1db6c18b6f019d2c7d
Josh Lukas <joshuah.lukas@gmail.com>
2020-02-02 16:36:48 -0500

    Fix heroku deployment

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 : "${PORT:=5000}"
+: "${HOST:="0.0.0.0"}"
 flask_dir="/app/instance/vidya.sqlite"
 
 if [ ! -e "${flask_dir}" ]; then
     flask init-db
 fi
 
-exec flask run -p "${PORT}"
+exec flask run -h "${HOST}" -p "${PORT}"
